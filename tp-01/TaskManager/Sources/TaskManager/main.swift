9643f3bca477c13c8502f89f639e4f64c7dd87ee
matteo113 <besancon.matteo@gmail.com>
2017-10-11 22:36:12 +0200

    comment and correct taskManger

--- tp-01/TaskManager/Sources/TaskManager/main.swift
+++ tp-01/TaskManager/Sources/TaskManager/main.swift
@@ -31,23 +31,42 @@ func base(){
     fatalError("unable to import place")
   }
 
-  guard let mark1 = create.fire(from: [taskPool: 0, processPool: 0, inProgress: 0]) else{fatalError("unable to fire transition")}
-  print("m1 \(mark1)")
-  guard let mark2 = spawn.fire(from: mark1) else{fatalError("unable to fire transition")}
-  print("m2 \(mark2)")
-  guard let mark3 = spawn.fire(from: mark2) else{fatalError("unable to fire transition")}
-  print("m3 \(mark3)")
-  guard let mark4 = exec.fire(from: mark3) else{fatalError("unable to fire transition")}
-  print("m4 \(mark4)")
-  guard let mark5 = exec.fire(from: mark4) else{fatalError("unable to fire transition")}
-  print("m5 \(mark5)")
-  guard let mark6 = success.fire(from: mark5) else{fatalError("unable to fire transition")}
-  print("m6 \(mark6)")
-  guard let mark7 = fail.fire(from: mark6) else{fatalError("unable to fire transition")}
-  print("m7 \(mark7)")
+  print("(CREATE) -->")
+  guard let mark1 = create.fire(from: [taskPool: 0, processPool: 0, inProgress: 0]) else{print("unable to fire transition"); return}
+  print("m1 = \(mark1)")
+  print()
+  print("(SPAWN) -->")
+  guard let mark2 = spawn.fire(from: mark1) else{print("unable to fire transition"); return}
+  print("m2 = \(mark2)")
+  print()
+  print("(SPAWN) -->")
+  guard let mark3 = spawn.fire(from: mark2) else{print("unable to fire transition"); return}
+  print("m3 = \(mark3)")
+  print()
+  print("(EXEC) -->")
+  guard let mark4 = exec.fire(from: mark3) else{print("unable to fire transition"); return}
+  print("m4 = \(mark4)")
+  print("As we can see here it is possible to fire EXEC a second time with the same task")
+  print()
+  print("(EXEC) -->")
+  guard let mark5 = exec.fire(from: mark4) else{print("unable to fire transition"); return}
+  print("m5 = \(mark5)")
+  print("Yet we have two times the same task inProgress but only one token in taskPool :(")
+  print()
+  print("(SUCCESS) -->")
+  guard let mark6 = success.fire(from: mark5) else{print("unable to fire transition"); return}
+  print("m6 = \(mark6)")
+  print("""
+  The poor lonely token is yet stuck. It's only way to escape this eternal trap is to fail
+  wich would be really sad for him because nobody likes to fail.
+  """)
+  print()
+  print("(FAIL) -->")
+  guard let mark7 = fail.fire(from: mark6) else{print("unable to fire transition"); return}
+  print("m7 = \(mark7)")
 }
 
-func correct(){
+func correct(error: Bool){
   guard let create = correctTaskManager.transitions.first(where: {$0.name == "create"}) else{
     fatalError("unable to import transition")
   }
@@ -58,9 +77,6 @@ func correct(){
     fatalError("unable to import transition")
   }
   guard let success = correctTaskManager.transitions.first(where: {$0.name == "success"}) else {
-    fatalError("unable to import transition")
-  }
-  guard let fail = correctTaskManager.transitions.first(where: {$0.name == "fail"}) else {
     fatalError("unable to import transition")
   }
 
@@ -77,21 +93,79 @@ func correct(){
       fatalError("unable to import place")
   }
 
-  guard let mark1 = create.fire(from: [taskPool: 0, processPool: 0, inProgress: 0, taskChecker: 0]) else{fatalError("unable to fire transition")}
+  print("(CREATE) -->")
+  guard let mark1 = create.fire(from: [taskPool: 0, processPool: 0, inProgress: 0, taskChecker: 0]) else{print("unable to fire transition"); return}
   print("m1 \(mark1)")
-  guard let mark2 = spawn.fire(from: mark1) else{fatalError("unable to fire transition")}
+  print()
+  print("(SPAWN) -->")
+  guard let mark2 = spawn.fire(from: mark1) else{print("unable to fire transition"); return}
   print("m2 \(mark2)")
-  guard let mark3 = spawn.fire(from: mark2) else{fatalError("unable to fire transition")}
+  print()
+  print("(SPAWN) -->")
+  guard let mark3 = spawn.fire(from: mark2) else{print("unable to fire transition"); return}
   print("m3 \(mark3)")
-  guard let mark4 = exec.fire(from: mark3) else{fatalError("unable to fire transition")}
+  print()
+  print("(EXEC) -->")
+  guard let mark4 = exec.fire(from: mark3) else{print("unable to fire transition"); return}
   print("m4 \(mark4)")
-  guard let mark5 = exec.fire(from: mark4) else{fatalError("unable to fire transition")}
+  print()
+  if error {
+    print("(EXEC) -->")
+    guard let mark5 = exec.fire(from: mark4) else{print("unable to fire transition"); return}
+    print("m5 \(mark5)")
+  }
+
+  print("(SUCCESS) -->")
+  guard let mark5 = success.fire(from: mark4) else{print("unable to fire transition"); return}
   print("m5 \(mark5)")
-  guard let mark6 = success.fire(from: mark5) else{fatalError("unable to fire transition")}
-  print("m6 \(mark6)")
-  guard let mark7 = fail.fire(from: mark6) else{fatalError("unable to fire transition")}
-  print("m7 \(mark7)")
 }
 
 base()
-correct()
+print("""
+
+**************************************************************************************************************************
+                .88888888:.
+               88888888.88888.
+             .8888888888888888.
+             888888888888888888
+             88' _`88'_  `88888
+             88 88 88 88  88888
+             88_88_::_88_:88888
+             88:::,::,:::::8888
+             88`:::::::::'`8888
+            .88  `::::'    8:88.
+           8888            `8:888.
+         .8888'             `888888.
+        .8888:..  .::.  ...:'8888888:.
+       .8888.'     :'     `'::`88:88888
+      .8888        '         `.888:8888.
+     888:8         .           888:88888
+   .888:88        .:           888:88888:
+   8888888.       ::           88:888888
+   `.::.888.      ::          .88888888
+  .::::::.888.    ::         :::`8888'.:.
+ ::::::::::.888   '         .::::::::::::
+ ::::::::::::.8    '      .:8::::::::::::.
+.::::::::::::::.        .:888:::::::::::::
+:::::::::::::::88:.__..:88888:::::::::::'
+ `'.:::::::::::88888888888.88:::::::::'
+     `':::_:' -- '' -'-' `':_::::'`
+**************************************************************************************************************************
+
+""")
+correct(error: true)
+
+print("""
+
+
+as we can se if we try the same pattern as previously it is not possible to fire EXEC with the same task a second time.
+
+**************************************************************************************************************************
+**************************************************************************************************************************
+
+Yet if we try the corect pattern it will perform as wanted.
+
+
+""")
+
+correct(error: false)
