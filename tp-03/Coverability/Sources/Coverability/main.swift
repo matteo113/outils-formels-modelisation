65c034ce9c9cc030d9cc638a86669202a364fe3a
matteo113 <besancon.matteo@gmail.com>
2017-11-09 20:31:13 +0100

    add main

--- tp-03/Coverability/Sources/Coverability/main.swift
+++ tp-03/Coverability/Sources/Coverability/main.swift
@@ -13,3 +13,24 @@ import CoverabilityLib
 // You **are** encouraged to write tests of your own!
 // You may write as many tests as you want here, or even better in `CoverabilityLibTests.swift`.
 // Observe how the two existing tests are implemented to write your own.
+
+let model = createBoundedModel()
+guard let r  = model.places.first(where: { $0.name == "r" }),
+      let p  = model.places.first(where: { $0.name == "p" }),
+      let t  = model.places.first(where: { $0.name == "t" }),
+      let m  = model.places.first(where: { $0.name == "m" }),
+      let w1 = model.places.first(where: { $0.name == "w1" }),
+      let s1 = model.places.first(where: { $0.name == "s1" }),
+      let w2 = model.places.first(where: { $0.name == "w2" }),
+      let s2 = model.places.first(where: { $0.name == "s2" }),
+      let w3 = model.places.first(where: { $0.name == "w3" }),
+      let s3 = model.places.first(where: { $0.name == "s3" })
+    else {
+        fatalError("invalid model")
+}
+
+let initialMarking: CoverabilityMarking = [r: 1, p: 0, t: 0, m: 0, w1: 1, s1: 0, w2: 1, s2: 0, w3: 1, s3: 0]
+
+let coverabilityGraph = model.coverabilityGraph(from: initialMarking)
+
+print(coverabilityGraph.count)
