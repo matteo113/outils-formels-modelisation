13216b386934c9257c7bf6d987b72862c7e8f6fb
matteo113 <besancon.matteo@gmail.com>
2017-12-06 20:35:20 +0100

    replace with old main

--- tp-05/ProofKit/Sources/ProofKit/main.swift
+++ tp-05/ProofKit/Sources/ProofKit/main.swift
@@ -2,27 +2,51 @@ import ProofKitLib
 
 let a: Formula = "a"
 let b: Formula = "b"
-let c: Formula = "c"
-let d: Formula = "d"
+let f = a && b
 
-let f = !(a && b)
-let f1 = a&&(b||c)
-let f2 = (!((a || d)||b))&&(b||(!c))
+print(f)
 
-print("Original : \(f)")
-print("NNF : \(f.nnf)")
-print("DNF : \(f.nnf.dnf)")
-print("CNF : \(f.nnf.cnf)")
-print("\n")
+let booleanEvaluation = f.eval { (proposition) -> Bool in
+    switch proposition {
+        case "p": return true
+        case "q": return false
+        default : return false
+    }
+}
+print(booleanEvaluation)
 
-print("Original : \(f1)")
-print("NNF : \(f1.nnf)")
-print("DNF : \(f1.nnf.dnf)")
-print("CNF : \(f1.nnf.cnf)")
-print("\n")
+enum Fruit: BooleanAlgebra {
 
-print("Original : \(f2)")
-print("NNF : \(f2.nnf)")
-print("DNF : \(f2.nnf.dnf)")
-print("CNF : \(f2.nnf.cnf)")
-print("\n")
+    case apple, orange
+
+    static prefix func !(operand: Fruit) -> Fruit {
+        switch operand {
+        case .apple : return .orange
+        case .orange: return .apple
+        }
+    }
+
+    static func ||(lhs: Fruit, rhs: @autoclosure () throws -> Fruit) rethrows -> Fruit {
+        switch (lhs, try rhs()) {
+        case (.orange, .orange): return .orange
+        case (_ , _)           : return .apple
+        }
+    }
+
+    static func &&(lhs: Fruit, rhs: @autoclosure () throws -> Fruit) rethrows -> Fruit {
+        switch (lhs, try rhs()) {
+        case (.apple , .apple): return .apple
+        case (_, _)           : return .orange
+        }
+    }
+
+}
+
+let fruityEvaluation = f.eval { (proposition) -> Fruit in
+    switch proposition {
+        case "p": return .apple
+        case "q": return .orange
+        default : return .orange
+    }
+}
+print(fruityEvaluation)
