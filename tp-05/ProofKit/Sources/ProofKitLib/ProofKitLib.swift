4d11840b32507640395d91b8130263506234361e
matteo113 <besancon.matteo@gmail.com>
2017-12-06 14:41:26 +0100

    TP5 finale version

--- tp-05/ProofKit/Sources/ProofKitLib/ProofKitLib.swift
+++ tp-05/ProofKit/Sources/ProofKitLib/ProofKitLib.swift
@@ -50,16 +50,16 @@ public enum Formula {
             return self
         case .negation(let a):
             switch a {
-            case .proposition(_):
-                return self
-            case .negation(let b):
-                return b.nnf
-            case .disjunction(let b, let c):
-                return (!b).nnf && (!c).nnf
-            case .conjunction(let b, let c):
-                return (!b).nnf || (!c).nnf
-            case .implication(_):
-                return (!a.nnf).nnf
+                case .proposition(_):
+                    return self
+                case .negation(let b):
+                    return b.nnf
+                case .disjunction(let b, let c):
+                    return (!b).nnf && (!c).nnf
+                case .conjunction(let b, let c):
+                    return (!b).nnf || (!c).nnf
+                case .implication(_):
+                    return (!a.nnf).nnf
             }
         case .disjunction(let b, let c):
             return b.nnf || c.nnf
@@ -72,13 +72,279 @@ public enum Formula {
 
     /// The disjunctive normal form of the formula.
     public var dnf: Formula {
-        // Write your code here ...
+
+        switch self {
+            case .proposition(_): // a
+                return self
+            case .negation(_):  // !a
+                return self
+
+            case .disjunction(let a, let b): // alpha v beta
+                switch a {
+                    case .proposition(_): // a v beta
+                        switch b {
+                            case.proposition(_): // a v b
+                                return self
+                            case.negation(_): // a v !b
+                                return self
+                            case.disjunction(_): // a v (b1 v b2)
+                                return (b || a).dnf
+                            case.conjunction(_): // a v (b1 ^ b2)
+                                return (b || a).dnf
+                            default : // Non NNF form
+                                fatalError("Unexpected value in DNF")
+                        }
+
+                    case .negation(_): // !a v beta
+                        switch b {
+                            case.proposition(_): // !a v b
+                                return (a || b)
+                            case.negation(_): // !a v !b
+                                return (a || b)
+                            case.disjunction(_): // !a v (b1 v b2)
+                                return (b || a).dnf
+                            case.conjunction(_): // !a v (b1 ^ b2)
+                                return (b || a).dnf
+                            default : // Non NNF form
+                                fatalError("Unexpected value in DNF")
+                        }
+
+                    case .disjunction(let a1, let a2): // (a1 v a2) v beta
+                        switch b {
+                            case.proposition(_): // (a1 v a2) v b
+                                return (a1.dnf || a2.dnf || b)
+                            case.negation(_): // (a1 v a2) v !b
+                                return (a1.dnf || a2.dnf || b)
+                            case.disjunction(_): // (a1 v a2) v (b1 v b2)
+                                return (a1.dnf || a2.dnf || b.dnf)
+                            case.conjunction(_): // (a1 v a2) v (b1 ^ b2)
+                                return (a1.dnf || a2.dnf || b.dnf)
+                            default : // Non NNF form
+                                fatalError("Unexpected value in DNF")
+                        }
+
+                    case .conjunction(_): // (a1 ^ a2) v beta (As it's already CNF, we don't need to use distributivity laws on a)
+                        switch b {
+                            case.proposition(_): // (a1 ^ a2) v b
+                                return (a.dnf ||  b)
+                            case.negation(_): // (a1 ^ a2) v !b
+                                return (a.dnf ||  b)
+                            case.disjunction(_): // (a1 ^ a2) v (b1 v b2)
+                                return (a.dnf ||  b.dnf)
+                            case.conjunction(_): // (a1 ^ a2) v (b1 ^ b2)
+                                return (a.dnf ||  b.dnf)
+                            default : // Non NNF form
+                                fatalError("Unexpected value in DNF")
+                        }
+
+                    default : // Non NNF form
+                        fatalError("Unexpected value in DNF")
+                }
+
+            case .conjunction(let a, let b): // alpha ^ beta
+                switch a {
+                    case .proposition(_): // a ^ beta
+                        switch b {
+                            case .proposition(_): // a ^ b
+                                return self
+                            case .negation(_): // a ^ !b
+                                return self
+                            case .disjunction(_): // a ^ (b1 v b2)
+                                return (b && a).dnf
+                            case .conjunction(_): // a ^ (b1 ^ b2)
+                                return (b && a).dnf
+                            default : // Non NNF Form
+                                fatalError("Unexpected value in DNF")
+                        }
+
+                    case .negation(_): // !a ^ beta
+                        switch b {
+                            case .proposition(_): // !a ^ b
+                                return (a && b)
+                            case .negation(_): // !a ^ !b
+                                return (a && b)
+                            case .disjunction(_): // !a ^ (b1 v b2)
+                                return (b && a).dnf
+                            case .conjunction(_): // !a ^ (b1 ^ b2)
+                                return (b && a).dnf
+                            default : // Non NNF Form
+                                fatalError("Unexpected value in DNF")
+                        }
+
+                    case .disjunction(let a1, let a2): // (a1 v a2) ^ (beta)
+                        switch b {
+                            case .proposition(_): // (a1 v a2) ^ b
+                                return ((a1.dnf && b).dnf || (a2.dnf && b).dnf)
+                            case .negation(_): // (a1 v a2) ^ !b
+                                return ((a1.dnf && b).dnf || (a2.dnf && b).dnf)
+                            case .disjunction(let b1, let b2): // (a1 v a2) ^ (b1 v b2)
+                                return ((a1.dnf && b1.dnf).dnf || (a1.dnf && b2.dnf).dnf || (a2.dnf && b1.dnf).dnf || (a2.dnf && b2.dnf).dnf)
+                            case .conjunction(let b1, let b2): // (a1 v a2) ^ (b1 ^ b2)
+                                return ((a1.dnf && b1.dnf && b2.dnf)||(a2.dnf && b1.dnf && b2.dnf).dnf)
+                            default : // Non NNF Form
+                                fatalError("Unexpected value in DNF")
+                        }
+
+                    case .conjunction(let a1, let a2): // (a1 ^ a2) ^ beta
+                        switch b {
+                            case .proposition(_): // (a1 ^ a2) ^ b
+                                return ((a1.dnf && a2.dnf).dnf && b)
+                            case .negation(_): // (a1 ^ a2) ^ !b
+                                return ((a1.dnf && a2.dnf).dnf && b)
+                            case .disjunction(let b1, let b2): // (a1 ^ a2) ^ (b1 v b2)
+                                return ((b1.dnf && a1.dnf && a2.dnf) || (b2.dnf && a1.dnf && a2.dnf))
+                            case .conjunction(let b1, let b2): /// (a1 ^ a2) ^ (b1 ^ b2)
+                                return ((a1.dnf && a2.dnf).dnf && (b1.dnf && b2.dnf).dnf)
+                            default : // Non NNF Form
+                                fatalError("Unexpected value in DNF")
+                        }
+                    default : // Non NNF Form
+                        fatalError("Unexpected value in DNF")
+                }
+            default : // Non NNF Form
+                fatalError("Unexpected value in DNF")
+        }
         return self
     }
 
     /// The conjunctive normal form of the formula.
     public var cnf: Formula {
-        // Write your code here ...
+
+        switch self {
+            case .proposition(_): // a
+                return self
+
+            case .negation(_): // !a
+                return self
+
+            case .disjunction(let a, let b): // alpha v beta
+                switch a {
+                    case .proposition(_): // a v beta
+                        switch b {
+                            case .proposition(_): // a v b
+                                return self
+                            case .negation(_): // a v !b
+                                return self
+                            case .disjunction(_): // a v (b1 v b2)
+                                return (b || a).cnf
+                            case .conjunction(_): // a v (b1 ^ b2)
+                                return (b || a).cnf
+                            default: // Non CNF Form
+                                fatalError("Unexpected value in CNF")
+                        }
+
+                    case .negation(_):
+                        switch b {
+                            case .proposition(_): // !a v b
+                                return self
+                            case .negation(_): // !a v !b
+                                return self
+                            case .disjunction(_): // !a v (b1 v b2)
+                                return (b || a).cnf
+                            case .conjunction(_): // !a v (b1 ^ b2)
+                                return (b || a).cnf
+                            default: // Non CNF Form
+                                fatalError("Unexpected value in CNF")
+                        }
+
+                    case .disjunction(let a1, let a2): // (a1 v a2) v beta
+                        switch b {
+                            case .proposition(_): // (a1 v a2) v b
+                                return ((a1.cnf || a2.cnf).cnf || b)
+                            case .negation(_): // (a1 v a2) v !b
+                                return ((a1.cnf || a2.cnf).cnf || b)
+                            case .disjunction(let b1, let b2): // (a1 v a2) v (b1 v b2)
+                                return ((a1.cnf || a2.cnf).cnf || (b1.cnf || b2.cnf).cnf) // FIXME -> a.cnf || b.cnf
+                            case .conjunction(let b1, let b2): // (a1 v a2) v (b1 ^ b2)
+                                return ((b1.cnf || a1.cnf || a2.cnf) && (b2.cnf || a1.cnf || a2.cnf))
+                            default: // Non CNF Form
+                                fatalError("Unexpected value in CNF")
+                        }
+
+                    case .conjunction(let a1, let a2): // (a1 ^ a2) v beta
+                        switch b {
+                            case .proposition(_): // (a1 ^ a2) v b
+                                return ((a1.cnf || b).cnf && (a2.cnf || b).cnf)
+                            case .negation(_): // (a1 ^ a2) v !b
+                                return ((a1.cnf || b).cnf && (a2.cnf || b).cnf)
+                            case .disjunction(let b1, let b2): // (a1 ^ a2) v (b1 v b2)
+                                return ((a1.cnf || b1.cnf || b2.cnf) && (a2.cnf || b1.cnf || b2.cnf))
+                            case .conjunction(let b1, let b2): // (a1 ^ a2) v (b1 ^ b2)
+                                return ((a1.cnf || b1.cnf).cnf && (a1.cnf || b2.cnf).cnf && (a2.cnf || b1.cnf).cnf && (a2.cnf || b2.cnf).cnf)
+                            default: // Non CNF Form
+                                fatalError("Unexpected value in CNF")
+                        }
+
+                    default: // Non CNF Form
+                        fatalError("Unexpected value in CNF")
+                }
+
+            case .conjunction(let a, let b):
+               switch a {
+                    case .proposition(_): // a ^ beta
+                        switch b {
+                            case .proposition(_): // a ^ b
+                                return self
+                            case .negation(_): // a ^ !b
+                                return self
+                            case .disjunction(_): // a ^ (b1 v b2)
+                                return (b && a).cnf
+                            case .conjunction(_): // a ^ (b1 ^ b2)
+                                return (b && a).cnf
+                            default: // Non CNF Form
+                                fatalError("Unexpected value in CNF")
+                        }
+
+                    case .negation(_):
+                        switch b {
+                            case .proposition(_): // !a ^ b
+                                return self
+                            case .negation(_): // !a ^ !b
+                                return self
+                            case .disjunction(_): // !a ^ (b1 v b2)
+                                return (b && a).cnf
+                            case .conjunction(_): // !a ^ (b1 ^ b2)
+                                return (b && a).cnf
+                            default: // Non CNF Form
+                                fatalError("Unexpected value in CNF")
+                        }
+
+                    case .disjunction(_): // (a1 v a2) ^ beta
+                        switch b {
+                            case .proposition(_): // (a1 v a2) ^ b
+                                return (a.cnf && b)
+                            case .negation(_): // (a1 v a2) ^ !b
+                                return (a.cnf && b)
+                            case .disjunction(_): // (a1 v a2) ^ (b1 v b2)
+                                return (a.cnf && b.cnf)
+                            case .conjunction(_): // (a1 v a2) ^ (b1 ^ b2)
+                                return (a.cnf && b.cnf)
+                            default: // Non CNF Form
+                                fatalError("Unexpected value in CNF")
+                        }
+
+                    case .conjunction(let a1, let a2): // (a1 ^ a2) ^ beta
+                        switch b {
+                            case .proposition(_): // (a1 ^ a2) ^ b
+                                return (a1.cnf && a2.cnf && b)
+                            case .negation(_): // (a1 ^ a2) ^ !b
+                                return (a1.cnf && a2.cnf && b)
+                            case .disjunction(_): // (a1 ^ a2) ^ (b1 v b2)
+                                return (a1.cnf && a2.cnf && b.cnf)
+                            case .conjunction(_): // (a1 ^ a2) ^ (b1 ^ b2)
+                                return (a1.cnf && a2.cnf && b.cnf)
+                            default: // Non CNF Form
+                                fatalError("Unexpected value in CNF")
+                        }
+
+                    default: // Non CNF Form
+                        fatalError("Unexpected value in CNF")
+                }
+
+            default:
+                fatalError("Unexpected value in CNF")
+        }
+
         return self
     }
 
